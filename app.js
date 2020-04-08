277dfd82cc77e797c40aeb4aed5cc8882e4986fb
analyn <analyncajocson@ymail.com>
2020-04-08 23:42:18 +0800

    added remote database connection

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
+const product = require('./routes/product.route');
 const app = express();
 
+let dev_db_url = '';
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
+mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise;
+
+let db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/products', product);
 let port = 3000;
 
 app.listen(port, () => {
